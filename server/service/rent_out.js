d10eb0f142b56904d7576453dbdc37927c475ad0
Kanyango <kariukikanyango@gmail.com>
2017-12-24 12:22:37 +0300

    Update rent_out.js

--- server/service/rent_out.js
+++ server/service/rent_out.js
@@ -1,1 +1,45 @@
 
+'use strict';
+var mongoose = require('mongoose');
+
+
+var rent_out = {
+
+	create : function(req , res , next)
+	{
+		var fieldsToSet = {
+
+			cname            : req.body.cname,
+			natId            : req.body.id,
+		  mail             : req.body.mail,
+			phone            : req.body.phone,
+			careg            : req.body.careg,
+		};
+    
+		
+		req.app.db.models.RentOut.create(fieldsToSet , 
+			function(err ,  docs){
+
+				if(err)
+				{
+					return next(err);
+				}
+				res.status(200).json(docs);
+			});
+	},
+	read : function(req , res , next)
+	{
+		
+		req.app.db.models.RentOut.find({},
+		    function(err , docs)
+			{
+				if(err)
+				{
+					return next(err);
+				}
+				res.status(200).json(docs);
+			});
+	}
+
+}
+module.exports = rent_out;
