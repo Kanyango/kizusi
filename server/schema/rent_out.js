e476c5d07859b281a32398953939bdf57a30a5cb
Kanyango <kariukikanyango@gmail.com>
2017-12-24 12:14:22 +0300

    Update rent_out.js

--- server/schema/rent_out.js
+++ server/schema/rent_out.js
@@ -1,1 +1,17 @@
+'use strict';
 
+module.exports = function(app , mongoose){
+
+	var rentOutSchema = new mongoose.Schema({
+		
+       //user       : {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+		dateCreated : {type : Date,default: Date.now()},
+		came  : {type: String},
+		careg : {type: String},
+		phone : {type: String},
+		mail  : {type: String},
+    natId  : {type: String},
+		
+	});
+	app.db.model('RentOut' , rentOutSchema);
+  };
