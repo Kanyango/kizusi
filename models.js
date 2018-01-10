031938883f9e0b12d445b68d5436c39ea3d1e723
Kanyango <kariukikanyango@gmail.com>
2018-01-10 11:20:43 +0300

    Update models.js

--- models.js
+++ models.js
@@ -8,4 +8,5 @@ module.exports = function(app , mongoose)
 	require('./server/schema/prices')(app , mongoose);
 	require('./server/schema/reserve')(app , mongoose);
 	require('./server/schema/rent_out')(app , mongoose);
+	require('./server/schema/notification')(app , mongoose);
 }
