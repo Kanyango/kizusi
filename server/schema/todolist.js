3f458166533f5ce87ab90878fd2001c34b4454ff
Kanyango <kariukikanyango@gmail.com>
2018-02-23 11:31:39 +0300

    Update todolist.js

--- server/schema/todolist.js
+++ server/schema/todolist.js
@@ -1,1 +1,16 @@
 
+module.exports = function(app , mongoose){
+
+	var todolistSchema = new mongoose.Schema({
+		
+       //user       : {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+		dateCreated : {type : Date,default: Date.now()},
+		title : {type: string},
+    owner : {type: string},
+    items : [], 
+    location: {type: string},
+    start_time : {type: string},
+    end_time : {type: string} 
+  });
+	app.db.model('ToDoList' , todolistSchema);
+  };
